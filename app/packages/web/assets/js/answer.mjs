bbd37eb92b3396781430906922bb25a74a17408b
AdrKacz <adrien.kaczmarek@gmail.com>
2024-03-18 14:08:09 +0000

    feat: quote dummy

--- app/packages/web/assets/js/answer.mjs
+++ app/packages/web/assets/js/answer.mjs
@@ -1,23 +1,42 @@
+// Fetch quote
+let isMacron = false // default value
+function getQuote() {
+    isMacron = true
+    return 'Nique tes morts !'
+}
+
+document.querySelector('#quote-paragraph').textContent = getQuote()
+
+// Set answers
 const macronButton = document.querySelector('#macron')
-const successModal = document.querySelector('#success-modal')
-const successModalCloseButton = document.querySelector('#success-modal .btn-close')
-
-macronButton.addEventListener('click', () => {
-    successModal.style.display = 'block'
-})
-
-successModalCloseButton.addEventListener('click', () => {
-    successModal.style.display = 'none'
-})
-
 const pasMacronButton = document.querySelector('#pas-macron')
+const successModal = document.querySelector('#success-modal')
 const failModal = document.querySelector('#fail-modal')
+const successModalCloseButton = document.querySelector('#success-modal .btn-close')
 const failModalCloseButton = document.querySelector('#fail-modal .btn-close')
 
+macronButton.addEventListener('click', () => {
+    if (isMacron) {
+        successModal.style.display = 'block'
+    } else {
+        failModal.style.display = 'block'
+    }
+})
+
 pasMacronButton.addEventListener('click', () => {
-    failModal.style.display = 'block'
+    if (!isMacron) {
+        successModal.style.display = 'block'
+    } else {
+        failModal.style.display = 'block'
+    }
+})
+
+// Close modals
+successModalCloseButton.addEventListener('click', () => {
+    successModal.style.display = 'none'
 })
 
 failModalCloseButton.addEventListener('click', () => {
     failModal.style.display = 'none'
 })
+
